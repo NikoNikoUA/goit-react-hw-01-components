661d6a61b4296ae4f9ee9e183f850cd33ef6d2ef
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-09-26 22:36:54 +0300

    hw 4 no css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from 'components/Profile/Profile'
 import { Statistics } from 'components/Statistics/Statistics'
-import { FriendList} from 'components/FriendList/FriendList'
+import { FriendList } from 'components/FriendList/FriendList'
+import { TransactionHistory} from 'components/TransactionHistory/TransactionHistory'
 import user from 'data-files/user.json';
 import data from 'data-files/data.json';
 import friends from 'data-files/friends.json';
 import transactions from '../data-files/transactions.json';
+
 
 export const App = () => {
   return (
@@ -14,11 +16,13 @@ export const App = () => {
     tag={user.tag}
     location={user.location}
     avatar={user.avatar}
-        stats={user.stats} />
+    stats={user.stats} />
       
       <Statistics title="Upload stats" statistics={data} />
       
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+export function TransactionHistory({ items }) {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, currency, amount }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+};
