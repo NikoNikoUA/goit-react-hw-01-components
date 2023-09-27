40a5f6e5e45c17b975948b818650a224e38ef83c
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-09-27 13:35:05 +0300

    HW 1 completed

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,8 +1,10 @@
 // import PropTypes from 'prop-types';
 
+import css from './TransactionHistory.module.css';
+
 export const TransactionHistory = ({ items }) => {
   return (
-    <table class="transaction-history">
+    <table className={css.transactionHistory}>
       <thead>
         <tr>
           <th>Type</th>
